f313a119d515de0d3557d22b5e6919cc222457e4
xinyiling <ling.303@osu.edu>
2024-10-28 17:22:23 -0400

    update

--- load_data.py
+++ load_data.py
@@ -11,8 +11,8 @@ from datasets import load_dataset
 import pdb
 
 def get_data(task, spl, is_gen_caps=False):
-    if os.path.exists(f'data/{task}_split.json'):
-        return json.load(open(f'data/{task}_split.json', 'r'))
+    if os.path.exists(f'data/{task}_{spl}.json'):
+        return json.load(open(f'data/{task}_{spl}.json', 'r'))
     df_data = pd.DataFrame(load_dataset("NingLab/MMECInstruct")['train'])
     df_data = df_data[(df_data['task'] == task) & (df_data['split'] == spl)]
     data = df_data.to_dict(orient='records')
@@ -42,7 +42,7 @@ def get_data(task, spl, is_gen_caps=False):
         new_entry['input'] = json.dumps(inp)
         new_entry['options'] = json.dumps(ops)
         new_data.append(new_entry)
-    json.dump(open(f'data/{task}_split.json', 'w'))
+    json.dump(open(f'data/{task}_{spl}.json', 'w'))
     return new_data
 
 if __name__ == '__main__':
